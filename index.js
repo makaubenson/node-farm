1e3a6a187bb3e4d6f8368c9b81c464457d65f491
Benson <bensonmakau2000@gmail.com>
2022-08-19 09:56:47 +0300

    reading file synchronously

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs"); //file system module
 
-const hello = "Hello World";
-console.log(hello);
+// const hello = "Hello World";
+// console.log(hello);
+
+//reading file synchronously
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);

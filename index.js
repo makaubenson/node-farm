b25d5dc961f9c72dc3df0927a6526647b2ce1ea7
Benson <bensonmakau2000@gmail.com>
2022-08-19 10:55:47 +0300

    error rendering-a

--- index.js
+++ index.js
@@ -15,13 +15,19 @@ fs.writeFileSync("./txt/output.txt", textOut);
 //reading file asynchronously
 fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   //   console.log(data1);
+  if (err) return console.log("ERROR!!");
   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
     console.log(data2);
     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
       console.log(data3);
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("File has been written");
-      });
+      fs.writeFile(
+        "./txt/final.txt",
+        `${data2} \n ${data3}`,
+        "utf-8",
+        (err) => {
+          console.log("File has been written");
+        }
+      );
     });
   });
 });
